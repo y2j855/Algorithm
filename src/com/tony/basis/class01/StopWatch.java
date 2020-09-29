5cee8f9550c1b05d62c4739dec877db811e993e3
y2j855 <y2j855@hotmail.com>
2020-09-29 14:50:38 +0800

    test run time

---
+++ src/com/tony/basis/class01/StopWatch.java
@@ -0,0 +1,19 @@
+package com.tony.basis.class01;
+
+/**
+ * @author: Tony.Chen
+ * Create Time : 2020/9/27 15:03
+ * Description:
+ */
+public class StopWatch {
+    private final long start;
+
+    public StopWatch() {
+        this.start = System.currentTimeMillis();
+    }
+
+    public double elapsedTime() {
+        long now = System.currentTimeMillis();
+        return (now - start) / 1000;
+    }
+}
