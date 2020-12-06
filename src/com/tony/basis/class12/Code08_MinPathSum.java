88948f2a4dee9685975879e125f197500f3a2049
y2j855 <y2j855@hotmail.com>
2020-12-06 12:57:21 +0800

    iterveiw question reboot move min path sum use dp

--- src/com/tony/basis/class12/Code08_MinPathSum.java
+++ src/com/tony/basis/class12/Code08_MinPathSum.java
@@ -1,12 +1,85 @@
 package com.tony.basis.class12;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author: Tony.Chen
  * Create Time : 2020/12/5 18:00
  * Description: 动态规划
- * 给定一个矩阵m，从左上角开始每次只能向右或者向下走，最后到达右下角的位置，路径上所有的数字累加起来就是路径和。返回所有的路径中最小的路径和
+ * 给定一个矩阵m，从左上角开始每次只能向右或者向下走，最后到达右下角的位置，路径上所有的数字累加起来就是路径和。
+ * 返回所有的路径中最小的路径和
  */
 public class Code08_MinPathSum {
+
+    public static int minPathSum(int[][] m) {
+        if (m == null || m.length == 0) {
+            return 0;
+        }
+        return process(m.length - 1, m[0].length - 1, m);
+    }
+
+    /**
+     * 暴力递归
+     *
+     * @param targetX 目标位置x坐标
+     * @param targetY 目标位置y坐标
+     * @param m       矩阵m
+     * @return 最小路径距离
+     */
+    private static int process(int targetX, int targetY, int[][] m) {
+        int row = m.length;
+        int column = m[0].length;
+        if (targetX < 0 || targetX >= row || targetY < 0 || targetY >= column) {
+            return 0;
+        }
+        if (targetX == 0 && targetY == 0) {
+            return m[targetX][targetY];
+        }
+        if (targetX == 0) {
+            return process(targetX, targetY - 1, m) + m[targetX][targetY];
+        }
+        if (targetY == 0) {
+            return process(targetX - 1, targetY, m) + m[targetX][targetY];
+        }
+        int left = process(targetX, targetY - 1, m);
+        int up = process(targetX - 1, targetY, m);
+
+        return Math.min(left, up) + m[targetX][targetY];
+    }
+
+    /**
+     * 记忆化搜索解决递归重复计算问题
+     * @param m
+     * @return
+     */
+    public static int minPathSumUseCache(int[][] m) {
+        if (m == null || m.length == 0) {
+            return 0;
+        }
+        Map<String, Integer> cache = new HashMap<>();
+        return processUseCache(m.length - 1, m[0].length - 1, m, cache);
+    }
+
+    private static int processUseCache(int targetX, int targetY, int[][] m, Map<String, Integer> cache) {
+        if (targetX == 0 && targetY == 0) {
+            return m[targetX][targetY];
+        }
+        if(cache.containsKey(targetX + "-" + targetY)){
+            return cache.get(targetX + "-" + targetY);
+        }
+        int result = 0;
+        if (targetX == 0) {
+            result = processUseCache(targetX, targetY - 1, m,cache) + m[targetX][targetY];
+        }else if (targetY == 0) {
+            result = processUseCache(targetX - 1, targetY, m,cache) + m[targetX][targetY];
+        }else{
+            result = Math.min(process(targetX, targetY - 1, m),
+                    processUseCache(targetX - 1, targetY, m,cache)) + m[targetX][targetY];
+        }
+        cache.put(targetX + "-" +targetY,result);
+        return result;
+    }
 
     /**
      * 动态规划第一版
@@ -55,6 +128,7 @@ public class Code08_MinPathSum {
      * 解题思路
      * 1.用一维数组替换二维数组，每遍历一行就代表当前行的值。
      * 2.可以再度优化，让空间更好，可以比较行列长度，设置一维数组的长度为长度短的。
+     *
      * @param m
      * @return
      */
@@ -105,5 +179,7 @@ public class Code08_MinPathSum {
         printMatrix(m1);
         System.out.println(dpWays(m1));
         System.out.println(dpWaysBest(m1));
+        System.out.println(minPathSum(m1));
+        System.out.println(minPathSumUseCache(m1));
     }
 }
